4e7814c7cd3d4bc835e0bcff188b717824641852
Juan <juan@pop-os.localdomain>
2020-06-05 13:49:31 -0400

    added social links

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import { graphql } from 'gatsby'
 import SEO from '../components/SEO'
 // ...GatsbyImageSharpFluid
 const IndexPage = () => {
-  return <Layout>home page</Layout>
+  return <Layout></Layout>
 }
 
 export default IndexPage
